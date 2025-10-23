60efc16d91f14565f59bbd123c7a12a0b6f9e932
Binaeg <newsletter.sibl@arcor.de>
2025-10-23 22:40:36 +0200

    fix: Correct import case for ItineraryTable component

--- app/page.js
+++ app/page.js
@@ -3,7 +3,7 @@
 import { useEffect, useState } from "react";
 import { useUser } from "@context/UserContext";
 import { useRouter } from "next/navigation";
-import ItineraryTable from "./components/itineraryTable";
+import ItineraryTable from "./components/ItineraryTable";
 
 export default function MyItinerariesPage() {
   const router = useRouter();
