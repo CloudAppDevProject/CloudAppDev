83907dc9e140f4011f5673c12ab15446408cef55
Binaeg <newsletter.sibl@arcor.de>
2025-11-29 11:41:46 +0100

    refactor: replace console logs with logger in LocationService and update main.ts logger configuration

--- services/travel-info-service/src/location/location.service.ts
+++ services/travel-info-service/src/location/location.service.ts
@@ -11,9 +11,9 @@ export class LocationService {
   async getCoordinatesByLocationName(
     locationName: string,
   ): Promise<{ lat: string; lon: string } | null> {
-    console.log(`Getting coordinates for location: ${locationName}`);
+    this.logger.log(`Getting coordinates for location: ${locationName}`);
     if (!locationName) {
-      console.log('Location name is required but not provided');
+      this.logger.warn('Location name is required but not provided');
       throw new BadRequestException('Location name is required');
     }
 
@@ -27,16 +27,16 @@ export class LocationService {
 
       if (Array.isArray(data) && data.length === 1) {
         const { lat, lon } = data[0];
-        console.log(
+        this.logger.log(
           `Found coordinates for ${locationName}: lat=${lat}, lon=${lon}`,
         );
         return { lat, lon };
       }
 
-      console.log(`No coordinates found for location: ${locationName}`);
+      this.logger.warn(`No coordinates found for location: ${locationName}`);
       return null;
     } catch (error) {
-      console.log(
+      this.logger.error(
         `Error fetching coordinates for ${locationName}: ${(error as any).message}`,
         (error as any).stack,
       );
@@ -48,9 +48,11 @@ export class LocationService {
     lat: number,
     lon: number,
   ): Promise<{ name: string } | null> {
-    console.log(`Getting city name for coordinates: lat=${lat}, lon=${lon}`);
+    this.logger.log(
+      `Getting city name for coordinates: lat=${lat}, lon=${lon}`,
+    );
     if (!lat || !lon) {
-      console.log('Latitude and longitude are required but not provided');
+      this.logger.warn('Latitude and longitude are required but not provided');
       throw new BadRequestException('Latitude and longitude are required');
     }
 
@@ -76,16 +78,18 @@ export class LocationService {
         data.display_name;
 
       if (cityName) {
-        console.log(
+        this.logger.log(
           `Found city name for coordinates (${lat}, ${lon}): ${cityName}`,
         );
         return { name: cityName };
       }
 
-      console.log(`No city name found for coordinates: lat=${lat}, lon=${lon}`);
+      this.logger.warn(
+        `No city name found for coordinates: lat=${lat}, lon=${lon}`,
+      );
       return null;
     } catch (error) {
-      console.log(
+      this.logger.error(
         `Error fetching city name for coordinates (${lat}, ${lon}): ${(error as any).message}`,
         (error as any).stack,
       );
