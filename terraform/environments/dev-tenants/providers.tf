9581a5b951c4a8c53e5cfd8e8cf94c0e4c621f31
Samuel Behrmann <samuel.behrmann@yahoo.de>
2026-01-12 20:38:21 +0100

    feat: Update provider versions in Terraform configuration for improved compatibility

--- terraform/environments/dev-tenants/providers.tf
+++ terraform/environments/dev-tenants/providers.tf
@@ -4,11 +4,15 @@ terraform {
   required_providers {
     google = {
       source  = "hashicorp/google"
-      version = "~> 6.0"
+      version = ">= 7.0"
     }
     cloudflare = {
       source  = "cloudflare/cloudflare"
-      version = "~> 4.0"
+      version = ">= 5.0"
+    }
+    kubernetes = {
+      source  = "hashicorp/kubernetes"
+      version = ">= 2.35"
     }
   }
 }
