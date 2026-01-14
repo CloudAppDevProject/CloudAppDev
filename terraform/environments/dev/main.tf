827e835dc4bc84e2e925ae525e9ae698a7f1c578
Samuel Behrmann <samuel.behrmann@yahoo.de>
2026-01-14 10:46:22 +0100

    feat: Add Kubernetes secrets for frontend and update ingress routes to allow all namespaces

--- terraform/environments/dev/main.tf
+++ terraform/environments/dev/main.tf
@@ -199,11 +199,21 @@ resource "kubernetes_manifest" "main_gateway" {
           name     = "http"
           protocol = "HTTP"
           port     = 80
+          allowedRoutes = {
+            namespaces = {
+              from = "All"
+            }
+          }
         },
         {
           name     = "https"
           protocol = "HTTPS"
           port     = 443
+          allowedRoutes = {
+            namespaces = {
+              from = "All"
+            }
+          }
         }
       ]
     }
